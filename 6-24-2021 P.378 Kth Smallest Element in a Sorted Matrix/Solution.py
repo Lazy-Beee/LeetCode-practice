ec15011a49152b37565f3a71374612061389da32
Lazy-Beee <37044764+fanj1@users.noreply.github.com>
2021-06-24 20:47:43 -0400

    6/24/2021

--- 6-24-2021 P.378 Kth Smallest Element in a Sorted Matrix/Solution.py
+++ 6-24-2021 P.378 Kth Smallest Element in a Sorted Matrix/Solution.py
@@ -1,6 +1,10 @@
 """
 Day 3   6/24/2021-Thursday
 Problem: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+Given an n x n matrix where each of the rows and columns are sorted in ascending order,
+return the  kth smallest element in the matrix.
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
 """
 
 
