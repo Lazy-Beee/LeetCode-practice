a9f11ed7e8a88cc8275cc99c9a57b79b428706e5
Lazy-Beee <37044764+fanj1@users.noreply.github.com>
2021-06-24 20:10:13 -0400

    6/24/2021

---
+++ 6-24-2021 P.378 Kth Smallest Element in a Sorted Matrix/fast_sol.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        lo, hi = matrix[0][0], matrix[-1][-1]
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if sum(bisect.bisect_right(row, mid) for row in matrix) < k:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo
